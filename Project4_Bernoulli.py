b6526d9c1a5b1a3591eeed2852af9dbb90b3e774
lalitayang <32180327+lalitayang@users.noreply.github.com>
2018-04-13 15:54:01 -0700

    Update Project4_Bernoulli.py

--- Project4_Bernoulli.py
+++ Project4_Bernoulli.py
@@ -15,27 +15,21 @@ def bernoulli_trials(n, p):
     """Perform n Bernoilli trials with success
     probability p and return number of successes"""
     
-    #initialize number of successes
     n_success = 0
         
     for i in range(n):
-        #call random number
         rand_n = np.random.random()
         
-        #if less than p, then success and add 1
         if rand_n < p:
             n_success += 1
     
     return n_success
     
-#initialize number of heads
 n_heads = np.empty(5000)
 
-#Compute number of heads in 5000 trials
 for i in range(5000):
     n_heads[i] = bernoulli_trials(1000, 0.5)
 
-#plot histogram
 plt.hist(n_heads)
 plt.xlabel('Number of heads out of 1000 flips')
 plt.ylabel('Number of trials out of 5000')
